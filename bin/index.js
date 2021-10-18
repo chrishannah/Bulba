664fa1d0df80102a54ebac3c07dcb6f64052664d
Christopher Hannah <me@chrishannah.me>
2021-10-18 15:31:25 +0100

    Add markdown conversion and template engine

--- bin/index.js
+++ bin/index.js
@@ -1,7 +1,9 @@
 #!/usr/bin/env node
 
-const gm = require('gray-matter')
+const greymatter = require('gray-matter')
 const fs = require('fs')
+const handlebars = require('handlebars')
+const marked = require('marked')
 
 const postDir = 'content/posts/';
 const filenames = fs.readdirSync(postDir);
@@ -10,14 +12,22 @@ console.log(filenames.length + ' file(s) found in posts directory');
 console.log('Reading files:');
 const posts = filenames.map(filename => {
     var file = fs.readFileSync(postDir + filename, 'utf8');
-    const {data:meta, content} = gm(file);
+    const { data: meta, content: markdown } = greymatter(file);
+    var content = marked(markdown);
     console.log(' - ' + meta.title + ' (' + filename + ')');
 
     return {
         meta,
         content
     }
-});
+})
 
+var postTemplateFilename = 'templates/post.hbs';
+var postTemplateFile = fs.readFileSync(postTemplateFilename, 'utf8');
+posts.forEach(post => {
+    var postTemplate = handlebars.compile(postTemplateFile);
+    var postPage = postTemplate(post);
+    fs.writeFileSync('out/' + post.meta.slug + '.html', postPage);
+});
 
 
