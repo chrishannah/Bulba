c444c1b7e00c1fbdc870f58835bfb339e450dbef
Christopher Hannah <me@chrishannah.me>
2021-10-18 14:17:14 +0100

    Read files from post directory

--- bin/index.js
+++ bin/index.js
@@ -3,17 +3,21 @@
 const gm = require('gray-matter')
 const fs = require('fs')
 
-const postDir = "content/posts/";
+const postDir = 'content/posts/';
 const filenames = fs.readdirSync(postDir);
-console.log(filenames.length + " file(s) found in posts directory");
+console.log(filenames.length + ' file(s) found in posts directory');
 
+console.log('Reading files:');
 const posts = filenames.map(filename => {
     var file = fs.readFileSync(postDir + filename, 'utf8');
     const {data:meta, content} = gm(file);
+    console.log(' - ' + meta.title + ' (' + filename + ')');
 
     return {
         meta,
         content
     }
 });
+
+
 
