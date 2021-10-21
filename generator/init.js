794598375da4494c6fce5eac072ce6ecd7338b1b
Christopher Hannah <me@chrishannah.me>
2021-10-21 14:15:46 +0100

    add example blog post to init command

--- generator/init.js
+++ generator/init.js
@@ -6,15 +6,20 @@ const { readProjectFile } = require('../tools/files')
 
 function initBlog() {
     // Create directories
-    fs.mkdirSync('content/posts', { recursive: true });
+    fs.mkdirSync('content/posts/', { recursive: true });
     fs.mkdirSync('assets', { recursive: true });
 
     // Create initial config file
     var configFile = readProjectFile('resources/blank-config.yaml');
     fs.writeFileSync('config.yaml', configFile);
 
-    // Create initial about file
-    fs.writeFileSync('content/about.md', "# About");
+    // Create initial config file
+    var aboutFile = readProjectFile('resources/example-about.md');
+    fs.writeFileSync('content/about.md', aboutFile);
+
+    // Create example blog post
+    var postFile = readProjectFile('resources/example-post.md');
+    fs.writeFileSync('content/posts/hello-world.md', postFile);
 }
 
 module.exports = {
---
+++ resources/example-about.md
@@ -0,0 +1,1 @@
+Hello, this is a page all about me.
